7fd38549540b406da9cc4a05badaea426fcfb7e4
manwanimonishashok <manwanimonishashok@gmail.com>
2019-04-26 12:48:53 +0530

    updated

--- week6/tennisplayer/players.js
+++ week6/tennisplayer/players.js
@@ -1,36 +1,43 @@
 'use strict';
 
-var express = require ("express");
+var express = require("express");
+
 
 var bodyParser = require("body-parser");
 
 var app = express();
 
+
 app.set("view engine", "hbs");
 
-app.use(bodyParser.urlencoded({ extended: false  }));
 
-var PLAYERS=[];
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/players", function (request, response){
+
+
+var DB = [];
+
+app.get("/", function(request, response){
+    
     var data = {
-      players: PLAYERS,
-      userName : "Monish",
-    };
-   response.render("players.hbs", data);
+        db: DB,
+        
+    }
+    response.render("players.hbs", data); 
 });
 
-app.post("/players", function(request, response) {
-    var playerName = request.body.playerName;
-    var playerLocation = request.body.playerLocation;
-    var details = {};
-    details.players = playerName;
-    details.location = playerLocation;
-    PLAYERS.push(details);
+app.post("/players", function(request, response){
     
+       
+        var data = {
+            name: request.body.name,
+            location: request.body.location,
+            
+        };
     
-   
-    response.redirect("/players");
+        DB.push(data);
+            
+        response.redirect("/"); 
 });
 
 app.listen(3000);
